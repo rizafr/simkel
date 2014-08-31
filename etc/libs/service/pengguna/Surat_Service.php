8daa46b6e6358b9c3819e0779c50aaddbb58fec5
rizafr <riza.fauzi.rahman@gmail.com>
2014-08-31 21:32:17 +0700

    update query belummenikah , update template andonnikah_template.rtf, update cetak belummenikahcetak.phtml

--- etc/libs/service/pengguna/Surat_Service.php
+++ etc/libs/service/pengguna/Surat_Service.php
@@ -652,7 +652,10 @@ class surat_Service {
 		$db = $registry->get('db');
 		try {
 			$db->setFetchMode(Zend_Db::FETCH_OBJ); 		
-				$result = $db->fetchRow("SELECT a.*, b.*, c.* FROM permintaan_belummenikah a, data_penduduk b, pejabat_kelurahan c WHERE  a.nik = b.nik AND a.id_pejabat = c.id_pejabat AND a.id_permintaan_belummenikah = $id_permintaan_belummenikah");
+				$result = $db->fetchRow("SELECT a.*, b.*, c.* , k.*, k.alamat as alamat_kelurahan, b.alamat as alamat_warga
+											FROM permintaan_belummenikah a, data_penduduk b, pejabat_kelurahan c , kelurahan k
+											WHERE  a.nik = b.nik AND a.id_pejabat = c.id_pejabat 
+											AND a.id_kelurahan=k.id_kelurahan AND a.id_permintaan_belummenikah = $id_permintaan_belummenikah");
 				return $result;
 		   } catch (Exception $e) {
 	         echo $e->getMessage().'<br>';
