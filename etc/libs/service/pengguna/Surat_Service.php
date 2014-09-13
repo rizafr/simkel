34eba9adff530a4ce118c0241e26431c28a2eb39
rizafr <riza.fauzi.rahman@gmail.com>
2014-09-13 13:14:08 +0700

    lama menunggu di andonnikahproses.phtml beres, modal notifikasi pengguna sudah,,,tp baru di andonnikah.phtml

--- etc/libs/service/pengguna/Surat_Service.php
+++ etc/libs/service/pengguna/Surat_Service.php
@@ -74,6 +74,77 @@
 			}
 		}
 		
+		 function selisih($time_1, $time_2){
+			date_default_timezone_set('Asia/Jakarta');
+			
+			$a = explode(":", $time_1);       
+			$b = explode(":", $time_2);          
+			
+			/* Explode parameter $time_1 */
+			$a_hour    = $a[0];
+			$a_minutes = $a[1];
+			$a_seconds = $a[2];
+			
+			/* Explode parameter $time_2 */
+			$b_hour    = $b[0];
+			$b_minutes = $b[1];
+			$b_seconds = $b[2];
+			
+			/* declare result variabel */
+			$c_hour    = NULL;
+			$c_minutes = NULL;
+			$c_seconds = NULL;
+			
+		   /* -----------------------------------------
+			* Pengurangan detik
+			* -----------------------------------------
+			**/
+			if($b_seconds >= $a_seconds)
+			{
+				$c_seconds = $b_seconds - $a_seconds;
+			}
+			else
+			{
+				$c_seconds = ($b_seconds + 60) - $a_seconds;
+				$b_minutes--;
+			}       
+			
+		   /* -----------------------------------------
+			* Pengurangan menit
+			* -----------------------------------------
+			**/
+			if($b_minutes >= $a_minutes)
+			{
+				$c_minutes = $b_minutes - $a_minutes;
+			}
+			else
+			{
+				$c_minutes = ($b_minutes + 60) - $a_minutes;
+				$b_hour--;
+			}       
+			
+		   /* -----------------------------------------
+			* Pengurangan jam
+			* -----------------------------------------
+			**/
+			if($b_hour >= $a_hour)
+			{
+				$c_hour = $b_hour - $a_hour;
+			}
+			else
+			{
+				$c_hour = '-' . ($a_hour - $b_hour);
+			}
+			
+			/* Checking time format */
+			if( strlen($c_seconds) == 1) $c_seconds = '0'.$c_seconds;
+			if( strlen($c_minutes) == 1) $c_minutes = '0'.$c_minutes;
+			if( strlen($c_hour) == 1) $c_hour = '0'.$c_hour;
+			
+			/* Return result */
+			return $c_hour . ':' . $c_minutes . ':' . $c_seconds;
+		}
+		
 		public function getSimpanNoRegistrasi(Array $data){
 			$registry = Zend_Registry::getInstance();
 			$db = $registry->get('db');
@@ -772,7 +843,7 @@
 				$db->setFetchMode(Zend_Db::FETCH_OBJ); 		
 				$result = $db->fetchAll("SELECT a.*, b.* FROM permintaan_andonnikah a, data_penduduk b 
 				WHERE a.id_kelurahan = $id_kelurahan AND a.nik = b.nik 
-				ORDER BY a.tanggal_surat desc, a.status desc,  a.no_registrasi DESC 
+				ORDER BY  a.no_registrasi DESC 
 				LIMIT $offset , $dataPerPage");
 				return $result;
 				} catch (Exception $e) {
