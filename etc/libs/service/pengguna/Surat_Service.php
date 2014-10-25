71939ef7d67b2b333f0ab388efb8d3c9b15834c7
ratihpujihati <ratih.pujihati@gmail.com>
2014-10-25 12:13:02 +0700

    beres cetak rumah sakit. service, format surat, cetak, tabel

--- etc/libs/service/pengguna/Surat_Service.php
+++ etc/libs/service/pengguna/Surat_Service.php
@@ -192,13 +192,16 @@
 		
 		
 		//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!rumah sakit
-		//cetak surat sekolah
-		public function getrumahsakitcetak($id_permintaan_rumahsakit){
+		//cetak surat Rumah sakit
+		public function getandonnikahcetak($id_permintaan_andonnikah){
 			$registry = Zend_Registry::getInstance();
 			$db = $registry->get('db');
 			try {
 				$db->setFetchMode(Zend_Db::FETCH_OBJ); 		
-				$result = $db->fetchRow("SELECT a.*, b.*, c.* FROM permintaan_rumahsakit a, data_penduduk b, pejabat_kelurahan c WHERE  a.nik = b.nik AND a.id_pejabat = c.id_pejabat AND a.id_permintaan_rumahsakit = $id_permintaan_rumahsakit");
+				$result = $db->fetchRow("SELECT a.*, b.*, c.* , k.*, k.alamat as alamat_kelurahan, b.alamat as alamat_warga
+										FROM permintaan_rumahsakit a, data_penduduk b, pejabat_kelurahan c, kelurahan k
+										WHERE  a.nik = b.nik AND a.id_pejabat = c.id_pejabat 
+										AND a.id_kelurahan=k.id_kelurahan AND a.id_permintaan_rumahsakit = $id_permintaan_rumahsakit");
 				return $result;
 				} catch (Exception $e) {
 				echo $e->getMessage().'<br>';
