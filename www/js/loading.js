bd44ff536ed3f6abfdd275ae617936c786ccc450
rizafr <riza.fauzi.rahman@gmail.com>
2014-11-03 09:02:55 +0700

    fix ktbajbcetak.phtml, mengurangi loading.js, fix header already sent IndexController.php

--- www/js/loading.js
+++ www/js/loading.js
@@ -1,6 +1,6 @@
 $(function(){
 		$(document.body).append('<div id="page-loader"></div>');
 	$(window).on("beforeunload", function() {
-		$('#page-loader').fadeIn(500).delay(9000).fadeOut(1000);
+		$('#page-loader').fadeIn(500).delay(3000).fadeOut(5000);
 	});
 });
